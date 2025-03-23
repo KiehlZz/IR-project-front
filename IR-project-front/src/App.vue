c4665a81d4a03fb8b24995f92efc49c3002a7c41
Sorod Yawa <sorod_yawa@cmu.ac.th>
2025-03-23 17:25:26 +0700

    set up tailwind

--- IR-project-front/src/App.vue
+++ IR-project-front/src/App.vue
@@ -4,21 +4,11 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="text-3xl text-blue-500 font-bold">
+    🍜 Hello from Tailwind v4!
+  </div>
 </template>
+
 
 <style scoped>
 header {
